c9c43f6da4149017417ed987070faf20b2dd5359
Leclowndu93150 <xdr.a123454321@gmail.com>
2024-07-07 20:00:32 +0200

    oops

--- src/main/java/com/leclowndu93150/carbort/registry/CreativeTabRegistry.java
+++ src/main/java/com/leclowndu93150/carbort/registry/CreativeTabRegistry.java
@@ -13,7 +13,7 @@ import static com.leclowndu93150.carbort.registry.ItemRegistry.*;
 public class CreativeTabRegistry {
     public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, Carbort.MODID);
 
-    public static final DeferredHolder<CreativeModeTab, CreativeModeTab> CARBORT_TAB = CREATIVE_MODE_TABS.register("angelring_tab", () -> CreativeModeTab.builder()
+    public static final DeferredHolder<CreativeModeTab, CreativeModeTab> CARBORT_TAB = CREATIVE_MODE_TABS.register("carbort_tab", () -> CreativeModeTab.builder()
             .title(Component.literal("Carbort")) //The language key for the title of your CreativeModeTab
             .withTabsBefore(CreativeModeTabs.COMBAT)
             .icon(() -> PARTY_PICKAXE.get().getDefaultInstance())
