bfa94cc0dcecb37d86383f7f805092c0cefe2b8c
ktpatient <167013520+ktpatient@users.noreply.github.com>
2024-07-10 12:56:22 +0100

    [Util] Automatically add all items to creative tab

--- src/main/java/com/leclowndu93150/carbort/registry/CreativeTabRegistry.java
+++ src/main/java/com/leclowndu93150/carbort/registry/CreativeTabRegistry.java
@@ -5,7 +5,9 @@ import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraft.world.item.Item;
 import net.neoforged.neoforge.registries.DeferredHolder;
+import net.neoforged.neoforge.registries.DeferredItem;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
 import static com.leclowndu93150.carbort.registry.ItemRegistry.*;
@@ -14,10 +16,13 @@ public class CreativeTabRegistry {
     public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, Carbort.MODID);
 
     public static final DeferredHolder<CreativeModeTab, CreativeModeTab> CARBORT_TAB = CREATIVE_MODE_TABS.register("carbort_tab", () -> CreativeModeTab.builder()
-            .title(Component.literal("Carbort")) //The language key for the title of your CreativeModeTab
+            .title(Component.literal("Carbort"))
             .withTabsBefore(CreativeModeTabs.COMBAT)
             .icon(() -> PARTY_PICKAXE.get().getDefaultInstance())
             .displayItems((parameters, output) -> {
-               output.accept(PARTY_PICKAXE.get());
+                for (DeferredHolder<Item, ? extends Item> item : ITEMS.getEntries()) {
+                    output.accept(item.get());
+                }
+               // output.accept(PARTY_PICKAXE.get());
             }).build());
 }
